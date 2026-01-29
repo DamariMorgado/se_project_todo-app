2f24c398921aa12ac913a1fbccbe7b2030d59969
Capyballer <dlmorgadomunoz@gmail.com>
2026-01-28 22:18:54 -0700

    Refactor Popup class for improved readability

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,23 +2,23 @@ class Popup {
   constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
   }
-
+ 
   open() {
     this._popupElement.classList.add("popup_visible");
     document.addEventListener("keyup", this._handleEscapeClose);
   }
-
+ 
   close() {
     this._popupElement.classList.remove("popup_visible");
     document.removeEventListener("keyup", this._handleEscapeClose);
   }
-
+ 
   _handleEscapeClose = (evt) => {
     if (evt.key === "Escape") {
       this.close();
     }
   };
-
+ 
   setEventListeners() {
     this._popupElement.addEventListener("mousedown", (evt) => {
       if (
@@ -30,5 +30,6 @@ class Popup {
     });
   }
 }
-
+ 
 export default Popup;
+ 
